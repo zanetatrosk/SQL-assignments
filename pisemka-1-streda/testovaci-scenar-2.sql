95b725212cdf8736f4d2774bc0ef2ab7625b0cb6
Žaneta Trošková <troskzan@fit.cvut.cz>
2023-03-29 16:09:04 +0200

    druhy ukol done

---
+++ pisemka-1-streda/testovaci-scenar-2.sql
@@ -0,0 +1,4 @@
+-- insert ktery se ma povest
+insert into PLAVBA_S_PRUVODCEM values (5,3,2,to_date('01.01.02','DD.MM.RR'));
+--insert ktery se nema povest
+insert into PLAVBA_S_PRUVODCEM values (4,8,1,to_date('01.01.01','DD.MM.RR'));
---
+++ pisemka-1-streda/ukol-2-implementace.sql
@@ -0,0 +1,21 @@
+create or replace TRIGGER zkontroluj_psp
+BEFORE INSERT OR UPDATE OR DELETE ON PLAVBA_S_PRUVODCEM
+FOR EACH ROW
+DECLARE
+    pocet_m integer;
+    pocet_max integer;
+    jmeno varchar2(20);
+BEGIN
+    pocet_max := 0;
+    select POCET_MIST into pocet_m from LOD where LODID = :new.ID_LOD;
+    DBMS_OUTPUT.PUT_LINE(pocet_m);
+    select max(POCET_MIST) into pocet_max from LOD join POKRYTI using (LODID) where NID = :new.ZID;
+    DBMS_OUTPUT.PUT_LINE(pocet_max);
+    select JMENON into jmeno from NAMORNIK where NAMORNIK.NID = :new.NID;
+    if pocet_max is null then
+        RAISE_APPLICATION_ERROR(-20002, 'Pro tuto plavbu není námořník ' || jmeno || 'klasifikován');
+    end if;
+    if pocet_max < pocet_m then
+        RAISE_APPLICATION_ERROR(-20002, 'Pro tuto plavbu není námořník ' || jmeno || 'klasifikován');
+    end if;
+END zkontroluj_psp;
